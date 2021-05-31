1a92d40241a64b1eabee0de47a019766312568a9
WaleedK2000 <74948618+WaleedK2000@users.noreply.github.com>
2021-05-31 15:53:33 +0500

    abcd

---
+++ e.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="src/ss/dbstyle.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+
+<body>
+
+
+    <div class="header">
+        <a href="#default" class="logo">CompanyLogo</a>
+        <div class="header-right">
+            <a class="active" href="#home">Home</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+    </div>
+
+
+    <div style="text-align:center;margin:auto;width:50%">
+        <div style=" float:left; PADDING-TOP:20px">
+            <form target="e.php" method="POST">
+                <label>Login Id</label>
+                <input type="text" name="logid">
+            </form>
+        </div>
+        <div style="PADDING-LEFT:20px;float:left">
+            <h1>Halalueih</h1>
+            <p>Die et sum bhej ghld ta da incno.</p>
+            <p>Some content..</p>
+        </div>
+    </div>
+
+
+
+
+
+</body>
+
+</html>
--- src/token.php
+++ src/token.php
@@ -6,17 +6,17 @@
 
 <body>
     <h1>Fit-me</h1>
-    <h2>Registration Form</h2>
+    <h2>token</h2>
 
-    <form action="token.php" method="GET">
-        <input type="text" name="f_name">
+    <form action="db-lab-proj/src/token.php" method="GET">
+        <input type="text">st
         <br>
         <input type="submit" value="Register">
     </form>
 
     <?
     session_start();
-    $_SESSION["favcolor"] = "4";
+    echo $_SESSION["favcolor"];
 
     ?>
 
